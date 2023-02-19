5e05bee654c67c3c9977a14b333422ee29b09449
VERTIGOJACK <vertigo.digital.design@gmail.com>
2023-02-19 18:37:50 +0100

    service registry template

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ import { CsvToTableAsync } from "./util/csvtotable/index.js";
 
 let pageindex = document.getElementById("pageindex");
 
-pageindex.innerHTML = await CsvToTableAsync("./csv/data.csv");
+pageindex.innerHTML = await CsvToTableAsync("./csv/data.csv", 5);
--- util/csvtotable/index.js
+++ util/csvtotable/index.js
@@ -1,15 +1,24 @@
 import * as csv from "../../csv-main/index.js";
 
-export const CsvToTableAsync = async (filePath) => {
+export const CsvToTableAsync = async (filePath, columns) => {
   const file = await fetch(filePath);
   const data = await file.text();
   const array = csv.parse(data);
   console.log(array);
-  const html = CommaDelimitedArrayToTable(array);
+  const html = CommaDelimitedArrayToTable(array, columns);
+  return html;
+};
+//börja här imorgon
+export const CsvToDescriptionAsync = async (filePath, columns) => {
+  const file = await fetch(filePath);
+  const data = await file.text();
+  const array = csv.parse(data);
+  console.log(array);
+  const html = CommaDelimitedArrayToTable(array, columns);
   return html;
 };
 
-const CommaDelimitedArrayToTable = (array) => {
+const CommaDelimitedArrayToTable = (array, columns) => {
   //enclosing table
   let result = "<table>";
 
@@ -19,26 +28,33 @@ const CommaDelimitedArrayToTable = (array) => {
     let data = array[i][0].split(";");
 
     //handle each element in a row
+    let columncounter = 0;
     data.forEach((element) => {
-      if (i == 0) {
-        result += "<th>";
-      } else {
-        result += "<td>";
-      }
-      //check if link
-      let checkhttp = element.split(":")[0];
-      if (checkhttp == "http" || checkhttp == "https") {
-        result += "<a href=";
-      }
-      //write content
-      result += element;
-      if (checkhttp == "http" || checkhttp == "https") {
-        result += ">Länk</a>";
-      }
-      if (i == 0) {
-        result += "</th>";
-      } else {
-        result += "</td>";
+      //switch special char for comma
+      let commasplit = element.split("%2c");
+      element = commasplit.join(",");
+      if (columncounter < columns) {
+        if (i == 0) {
+          result += "<th>";
+        } else {
+          result += "<td>";
+        }
+        //check if link
+        let checkhttp = element.split(":")[0];
+        if (checkhttp == "http" || checkhttp == "https") {
+          result += "<a href=";
+        }
+        //write content
+        result += element;
+        if (checkhttp == "http" || checkhttp == "https") {
+          result += ">Länk</a>";
+        }
+        if (i == 0) {
+          result += "</th>";
+        } else {
+          result += "</td>";
+        }
+        columncounter++;
       }
     });
     result += "</tr>";
