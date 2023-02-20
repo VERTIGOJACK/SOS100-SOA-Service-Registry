db182e89d9a72099b5be049a445d0fd3fa8ef39f
VERTIGOJACK <vertigo.digital.design@gmail.com>
2023-02-20 10:55:51 +0100

    fist commit to old branch

--- index.js
+++ index.js
@@ -2,4 +2,10 @@ import { CsvToTableAsync } from "./util/csvtotable/index.js";
 
 let pageindex = document.getElementById("pageindex");
 
-pageindex.innerHTML = await CsvToTableAsync("./csv/data.csv", 5);
+pageindex.innerHTML = await CsvToTableAsync("./csv/data2.csv", 5);
+
+//then construct items from excel sheet- maybe turn into json object ?
+//also maybe add "details" and link anchor to id on page. csv reader is getting real spaghetti now lol
+
+//ok i guess fucking scrap all this and start over with a google sheets and load in a JSON endpoint
+//treat json in a function and go banaynay
--- util/csvtotable/index.js
+++ util/csvtotable/index.js
@@ -9,12 +9,12 @@ export const CsvToTableAsync = async (filePath, columns) => {
   return html;
 };
 //börja här imorgon
-export const CsvToDescriptionAsync = async (filePath, columns) => {
+export const CsvToDetailedItemAsync = async (filePath) => {
   const file = await fetch(filePath);
   const data = await file.text();
   const array = csv.parse(data);
   console.log(array);
-  const html = CommaDelimitedArrayToTable(array, columns);
+  const html = CommaDelimitedArrayToItem(array);
   return html;
 };
 
@@ -33,7 +33,7 @@ const CommaDelimitedArrayToTable = (array, columns) => {
       //switch special char for comma
       let commasplit = element.split("%2c");
       element = commasplit.join(",");
-      if (columncounter < columns) {
+      if (columncounter < columns + 1) {
         if (i == 0) {
           result += "<th>";
         } else {
@@ -44,9 +44,20 @@ const CommaDelimitedArrayToTable = (array, columns) => {
         if (checkhttp == "http" || checkhttp == "https") {
           result += "<a href=";
         }
+        //check if details
+        if (columncounter == columns && i != 0) {
+          result += "<a href=#service" + i;
+        }
         //write content
-        result += element;
-        if (checkhttp == "http" || checkhttp == "https") {
+        if (columncounter != columns || i == 0) {
+          result += element;
+        }
+
+        if (
+          checkhttp == "http" ||
+          checkhttp == "https" ||
+          (columncounter == columns && i != 0)
+        ) {
           result += ">Länk</a>";
         }
         if (i == 0) {
@@ -60,5 +71,59 @@ const CommaDelimitedArrayToTable = (array, columns) => {
     result += "</tr>";
   }
   result += "</table>";
+  return result;
+};
+
+const CommaDelimitedArrayToItem = (array, columns) => {
+
+  for (let i = 0; i < array.length; i++) {
+    //enclosing row
+    result += "<section id=service"+i+">";
+    let data = array[i][0].split(";");
+
+    //handle each element in a row
+    let columncounter = 0;
+    data.forEach((element) => {
+      //switch special char for comma
+      let commasplit = element.split("%2c");
+      element = commasplit.join(",");
+      if (columncounter < columns + 1) {
+        if (i == 0) {
+          result += "<th>";
+        } else {
+          result += "<td>";
+        }
+        //check if link
+        let checkhttp = element.split(":")[0];
+        if (checkhttp == "http" || checkhttp == "https") {
+          result += "<a href=";
+        }
+        //check if details
+        if (columncounter == columns && i != 0) {
+          result += "<a href=#service" + i;
+        }
+        //write content
+        if (columncounter != columns || i == 0) {
+          result += element;
+        }
+
+        if (
+          checkhttp == "http" ||
+          checkhttp == "https" ||
+          (columncounter == columns && i != 0)
+        ) {
+          result += ">Länk</a>";
+        }
+        if (i == 0) {
+          result += "</th>";
+        } else {
+          result += "</td>";
+        }
+        columncounter++;
+      }
+    });
+    result += "</tr>";
+  }
+  result += "</section>";
   return result;
 };
