33b6a0cd1044956b18cf22b78fc0f0c3c6ec0e28
VERTIGOJACK <vertigo.digital.design@gmail.com>
2023-03-05 13:32:45 +0100

    added loading spinner

--- index.js
+++ index.js
@@ -6,13 +6,28 @@ import { workgroupsList } from "./util/data/data.js";
 let groupsContainer = document.getElementById("groups-container");
 let indexContainer = document.getElementById("index-container");
 let content = document.getElementById("content");
+let loadingCircles = document.querySelectorAll(".loading-circle");
+
+//load spinner from html
+const parser = new DOMParser();
+let response = await fetch("./util/loading-spinner-4/dist/index.html");
+let text = await response.text();
+
+loadingCircles.forEach((element) => {
+  const spinner = parser
+    .parseFromString(text, "text/html")
+    .querySelector(".spinner-container");
+  element.appendChild(spinner);
+});
 
 const json = await ServiceImportJson();
 const table = JsonToTable(json);
 const items = JsonToItems(json);
 const groups = JsonToWorkGroupsTable(workgroupsList);
 
-console.log(items);
+loadingCircles.forEach((element) => {
+  element.remove();
+});
 
 indexContainer.appendChild(table);
 content.appendChild(items);
